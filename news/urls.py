c4ebfc006865d57d8011f67528a30edf47a9a04f
Andrii Zadorozhnii <zador746@gmail.com>
2024-12-28 19:20:02 +0200

    dinamicaly changeble pages

--- news/urls.py
+++ news/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path
 
 from . import views
+# from .views import NewsDetailView
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news-detail')
 ]
